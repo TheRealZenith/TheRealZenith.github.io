00de970188150e4b4767ccc0ec724111bc282920
TheRealZenith <87614482+TheRealZenith@users.noreply.github.com>
2022-07-05 23:29:16 +0100

    Initial commit

---
+++ uhoh.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>uh oh</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php $ip_address = $_SERVER["REMOTE_ADDR"];     // user ip adderss ?>
+  <marquee>
+    <h1><?php echo $ip_address?> </h1>
+  </marquee>
+  <div></div>
+  <img class="tile--img__img  js-lazyload" src="//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rwLa13db-ndsY1gKOgHG1AHaHW%26pid%3DApi&amp;f=1" data-src="//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rwLa13db-ndsY1gKOgHG1AHaHW%26pid%3DApi&amp;f=1" alt="echo | famous chihuahua" width ="1000" height = "500">
+ 
+</body>
+
+</html>
